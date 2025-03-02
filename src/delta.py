beb7113fc7d80a967974034c6eed085b67a01b5c
Jourdain <leo.jourdain@universite-paris-saclay.fr>
2025-03-02 16:27:24 +0100

    [ADD] entry point for the project with cmd parameters parsing

---
+++ src/delta.py
@@ -0,0 +1,7 @@
+import utils
+
+def delta(vcf: list):
+
+    print(vcf)
+    
+    assert len(vcf) == 2, "Two VCF files are required"
---
+++ src/main.py
@@ -0,0 +1,10 @@
+from cli import Programm
+
+def main():
+
+    prog = Programm()
+    prog.launch()
+
+if __name__ == "__main__":
+    main()
+
