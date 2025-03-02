6d76a5286d9a49771cfc6d2eb0f91dd189c48083
Jourdain <leo.jourdain@universite-paris-saclay.fr>
2025-03-02 19:50:36 +0100

    [ADD] bash script for indexing vcf files

---
+++ src/indexing.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+which bgzip > /dev/null
+bgzip=$?
+
+which tabix > /dev/null
+tabix=$?
+
+which bcftools > /dev/null
+bcftools=$?
+
+indexing(){
+    local input="$1"
+    local output="$1.gz"
+
+    if [[ $bgzip -eq 0  && $tabix -eq 0]]; then
+        bgzip -c $input > $output && tabix -p vcf $output
+    elif [ $bcftools -eq 0 ]; then
+        bcftools view -O z -o $output $input && bcftools index $output
+    fi
+    if [ $? -ne 0 ]; then
+        exit 1
+    fi
+}
+
+indexing $1
+indexing $2
+
