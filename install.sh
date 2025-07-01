df454dc09f00836344d3462aaf7f1f5bc96504cd
leooojr <leo.jourdain@aphp.fr>
2025-07-01 15:52:20 +0200

    [UPDATE] install.sh with INSTALL variable

--- install.sh
+++ install.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
 
-PROJECT_DIR=${PWD}
+INSTALL_DIR=$1
+CURRENT_DIR=$(pwd)
 
-if [ ! -d src/htslib ]; then
-    mkdir ${PROJECT_DIR}/src/htslib
+if [ ! -d ${INSTALL_DIR}/src/htslib ]; then
+    mkdir -p ${INSTALL_DIR}/src/htslib
 fi
 
-(cd ${PROJECT_DIR}/htslib-1.21/ && \
-./configure --prefix ${PROJECT_DIR}/src/htslib/ && \
+(cd ${CURRENT_DIR}/htslib-1.21/ && \
+./configure --prefix ${INSTALL_DIR}/src/htslib/ && \
 make && \
 make install) && \
-rm -rf ${PROJECT_DIR}/htslib-1.21/
+rm -rf ${CURRENT_DIR}/htslib-1.21/
