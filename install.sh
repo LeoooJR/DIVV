3d08dfb2781152e3def72c9f4da5b992ed62a7f5
leooojr <leo.jourdain@aphp.fr>
2025-07-04 11:20:44 +0200

    [UPDATE] install path

--- install.sh
+++ install.sh
@@ -3,12 +3,12 @@
 INSTALL_DIR=$1
 CURRENT_DIR=$(pwd)
 
-if [ ! -d ${INSTALL_DIR}/src/htslib ]; then
-    mkdir -p ${INSTALL_DIR}/src/htslib
+if [ ! -d ${INSTALL_DIR}/htslib ]; then
+    mkdir -p ${INSTALL_DIR}/htslib
 fi
 
 (cd ${CURRENT_DIR}/htslib-1.21/ && \
-./configure --prefix ${INSTALL_DIR}/src/htslib/ && \
+./configure --prefix ${INSTALL_DIR}/htslib/ && \
 make && \
 make install) && \
 rm -rf ${CURRENT_DIR}/htslib-1.21/
